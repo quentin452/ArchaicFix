85a348c590d282fa9ee177b12a38f869142f8084
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-10-16 21:03:05 +0200

    update OcclusionRenderer.java

--- src/main/java/org/embeddedt/archaicfix/occlusion/OcclusionRenderer.java
+++ src/main/java/org/embeddedt/archaicfix/occlusion/OcclusionRenderer.java
@@ -236,18 +236,18 @@ public class OcclusionRenderer {
     }
 
     private boolean rebuildChunks(EntityLivingBase view, long deadline) {
-        int updateLimit = deadline == 0 ? 5 : Integer.MAX_VALUE;
+        int updateLimit = (deadline == 0) ? 5 : Integer.MAX_VALUE;
         int updates = 0;
-
         boolean spareTime = true;
+
         deferNewRenderUpdates = true;
         rendererUpdateOrderProvider.prepare(worldRenderersToUpdateList);
-        for (int c = 0; updates < updateLimit && rendererUpdateOrderProvider.hasNext(worldRenderersToUpdateList); ++c) {
-            WorldRenderer worldrenderer = rendererUpdateOrderProvider.next(worldRenderersToUpdateList);
-            
-            ((IWorldRenderer)worldrenderer).arch$setInUpdateList(false);
 
-            if (!(worldrenderer.isInFrustum & worldrenderer.isVisible) && !OcclusionHelpers.DEBUG_LAZY_CHUNK_UPDATES) {
+        while (updates < updateLimit && rendererUpdateOrderProvider.hasNext(worldRenderersToUpdateList)) {
+            WorldRenderer worldrenderer = rendererUpdateOrderProvider.next(worldRenderersToUpdateList);
+            ((IWorldRenderer) worldrenderer).arch$setInUpdateList(false);
+
+            if (!worldrenderer.isInFrustum || !worldrenderer.isVisible || (OcclusionHelpers.DEBUG_LAZY_CHUNK_UPDATES && !worldrenderer.isWaitingOnOcclusionQuery)) {
                 continue;
             }
 
@@ -256,10 +256,10 @@ public class OcclusionRenderer {
             worldrenderer.isVisible &= !e;
             worldrenderer.isWaitingOnOcclusionQuery = worldrenderer.skipAllRenderPasses() || (mc.theWorld.getChunkFromBlockCoords(worldrenderer.posX, worldrenderer.posZ) instanceof EmptyChunk);
             // can't add fields, re-use
-
-            if(worldrenderer.distanceToEntitySquared(view) > 272f) {
+            if (worldrenderer.distanceToEntitySquared(view) > 272f) {
                 updates++;
-                if(!worldrenderer.isWaitingOnOcclusionQuery || deadline != 0 || OcclusionHelpers.DEBUG_LAZY_CHUNK_UPDATES) {
+
+                if (!worldrenderer.isWaitingOnOcclusionQuery || deadline != 0 || OcclusionHelpers.DEBUG_LAZY_CHUNK_UPDATES) {
                     long t = System.nanoTime();
                     if (t > deadline) {
                         spareTime = false;
@@ -268,6 +268,7 @@ public class OcclusionRenderer {
                 }
             }
         }
+
         rendererUpdateOrderProvider.cleanup(worldRenderersToUpdateList);
         deferNewRenderUpdates = false;
         return spareTime;
